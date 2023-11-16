a8f4a65f11cee7bfea61c1ee32fd97bf2dc535c4
Software Rockstar <mharoon@softwarerockstar.com>
2023-11-16 14:51:43 -0600

    working on responsive tabs

--- src/app/shared/components/responsive-tabs/responsive-tabs.component.ts
+++ src/app/shared/components/responsive-tabs/responsive-tabs.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../modules/material/material.module';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { distinctUntilChanged, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'responsive-tabs',
@@ -9,6 +11,35 @@ import { MaterialModule } from '../../modules/material/material.module';
   templateUrl: './responsive-tabs.component.html',
   styleUrl: './responsive-tabs.component.scss'
 })
-export class ResponsiveTabsComponent {
+export class ResponsiveTabsComponent implements OnInit {
+  Breakpoints = Breakpoints;
+  currentBreakpoint:string = '';
+  
+  readonly breakpoint$ = this.breakpointObserver
+    .observe([Breakpoints.Large, Breakpoints.Medium, Breakpoints.Small, '(min-width: 500px)'])
+    .pipe(
+      tap(value => console.log(value)),
+      distinctUntilChanged()
+    );
+    
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
+  ngOnInit(): void {
+    this.breakpoint$.subscribe(() =>
+      this.breakpointChanged()
+    );
+  }
+
+
+  private breakpointChanged() {
+    if(this.breakpointObserver.isMatched(Breakpoints.Large)) {
+      this.currentBreakpoint = Breakpoints.Large;
+    } else if(this.breakpointObserver.isMatched(Breakpoints.Medium)) {
+      this.currentBreakpoint = Breakpoints.Medium;
+    } else if(this.breakpointObserver.isMatched(Breakpoints.Small)) {
+      this.currentBreakpoint = Breakpoints.Small;
+    } else if(this.breakpointObserver.isMatched('(min-width: 500px)')) {
+      this.currentBreakpoint = '(min-width: 500px)';
+    }
+  }
 }
