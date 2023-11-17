adf4abb422f68cae4983f6c8fa0ae5fb5e743ff1
Software Rockstar <mharoon@softwarerockstar.com>
2023-11-17 09:25:05 -0600

    working on responsive-tabs

--- src/app/shared/components/responsive-tabs/responsive-tabs.component.ts
+++ src/app/shared/components/responsive-tabs/responsive-tabs.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../modules/material/material.module';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { distinctUntilChanged, tap } from 'rxjs/operators';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+
+export interface ResponsiveTabsModel {
+  label: string;
+  value?: string;
+}
 
 @Component({
   selector: 'responsive-tabs',
@@ -12,6 +18,17 @@ import { distinctUntilChanged, tap } from 'rxjs/operators';
   styleUrl: './responsive-tabs.component.scss'
 })
 export class ResponsiveTabsComponent implements OnInit {
+
+  @Input() tabs: ResponsiveTabsModel[] = [
+    { label: 'First', value: '0' },
+    { label: 'Second', value: '1' },
+    { label: 'Third', value: '2' },
+    { label: 'Fourth', value: '3' },
+    { label: 'Fifth', value: '4' }
+  ];
+
+  @Input() responsiveText: string = "Please click to see options";
+
   Breakpoints = Breakpoints;
   currentBreakpoint:string = '';
   
@@ -42,4 +59,19 @@ export class ResponsiveTabsComponent implements OnInit {
       this.currentBreakpoint = '(min-width: 500px)';
     }
   }
+
+
+  onTabChange (event: MatTabChangeEvent): void {
+    //const me = this;
+
+    console.log(event.tab.textLabel)
+    //There I should get tab component reference
+
+  }
+
+  menuItemClick(args: Event) {
+    console.log(args.target);
+  }
+
+  
 }
