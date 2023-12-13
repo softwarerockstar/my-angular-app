813b9baf3c5211a6e3c690e1496d41236422a4d2
Software Rockstar <mharoon@softwarerockstar.com>
2023-12-13 15:45:45 -0600

    modified after merge

--- src/app/shared/components/demo-shared-components/demo-shared-components.component.ts
+++ src/app/shared/components/demo-shared-components/demo-shared-components.component.ts
@@ -31,7 +31,7 @@ import { SimplePopupComponent, SimplePopupActionButtonComponent } from '../simpl
         TooltipIconComponent,
         MultiItemLinkButtonComponent,
         HyperTabsModule,
-    DeliveryPreferencesComponent,
+        DeliveryPreferencesComponent,
         SimplePopupComponent,
         SimplePopupActionButtonComponent
     ]
