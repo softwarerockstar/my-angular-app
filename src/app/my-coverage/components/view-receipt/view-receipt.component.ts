960fa199e0bafcf33ffcf1d79f966fe6d674a331
Software Rockstar <mharoon@softwarerockstar.com>
2024-01-20 14:37:33 -0600

    finished view-receipt responsive layout

--- src/app/my-coverage/components/view-receipt/view-receipt.component.ts
+++ src/app/my-coverage/components/view-receipt/view-receipt.component.ts
@@ -4,6 +4,7 @@ import { InfoCardComponent } from 'src/app/shared/components/info-card/info-card
 import { MaterialModule } from 'src/app/shared/modules/material/material.module';
 import {Router} from '@angular/router';
 import { FileActionButtonComponent } from 'src/app/shared/components/buttons/file-action-button/file-action-button.component';
+import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
 
 export interface MyCoverageViewReceiptModel {
   submittedOn: string,
@@ -25,8 +26,22 @@ export interface MyCoverageViewReceiptModel {
 })
 export class MyCoverageViewReceiptComponent {
 
-  constructor(private router: Router) {}
+  isSmallScreen = false;
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver) { }
+  
+  ngOnInit(): void {
+    this.breakpointObserver.observe(Breakpoints.HandsetPortrait)    
+      .subscribe((state: BreakpointState) => {
+        if (state.matches)
+          this.isSmallScreen = true;
+        else
+          this.isSmallScreen = false;    
+      });  
+  }  
 
+  // TODO: remove fake data
+  // TODO: remove console.log debugs
+  
   documents: MyCoverageViewReceiptModel[] = [
     {submittedOn: '11/29/2023 06:03:15 PM', status: 'Submitted', documentHref: '#'},
     {submittedOn: '10/29/2023 06:02:15 PM', status: 'Submitted', documentHref: '#'},
@@ -40,6 +55,11 @@ export class MyCoverageViewReceiptComponent {
     console.log('navigating docHref');
     this.router.navigateByUrl(docHref);
   }
+
+  viewDetails() {
+    console.log('View Details clicked');
+  }
+
 }
 
 
