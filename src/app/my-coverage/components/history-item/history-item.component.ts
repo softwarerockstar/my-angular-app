e162d6f94b5f2291184454db9c5e899981795d3b
Software Rockstar <mharoon@softwarerockstar.com>
2024-01-29 11:37:58 -0600

    working on history

--- src/app/my-coverage/components/history-item/history-item.component.ts
+++ src/app/my-coverage/components/history-item/history-item.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/shared/modules/material/material.module';
 import { SingleItemLinkButtonComponent } from 'src/app/shared/components/single-item-link-button/single-item-link-button.component';
+import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'my-coverage-history-item',
@@ -16,6 +18,20 @@ import { SingleItemLinkButtonComponent } from 'src/app/shared/components/single-
 })
 export class HistoryItemComponent {
   panelOpenState = true;
+
+  isSmallScreen = false;
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver) { }
+  
+  ngOnInit(): void {
+    this.breakpointObserver.observe(Breakpoints.HandsetPortrait)    
+      .subscribe((state: BreakpointState) => {
+        if (state.matches)
+          this.isSmallScreen = true;
+        else
+          this.isSmallScreen = false;    
+      });  
+  }  
+
   
   @Input({required: true}) accountType: string = '';
   @Input({required: true}) accountName: string = '';
