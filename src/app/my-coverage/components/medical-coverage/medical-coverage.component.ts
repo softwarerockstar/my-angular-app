8da1f1fba578446be79c91d7b03c00f0fffcaca7
Software Rockstar <mharoon@softwarerockstar.com>
2024-01-30 09:49:55 -0600

    medical coverage policy responsive

--- src/app/my-coverage/components/medical-coverage/medical-coverage.component.ts
+++ src/app/my-coverage/components/medical-coverage/medical-coverage.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SingleItemLinkButtonComponent } from 'src/app/shared/components/single-item-link-button/single-item-link-button.component';
 import { MaterialModule } from 'src/app/shared/modules/material/material.module';
+import { Router } from '@angular/router';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'medical-coverage',
@@ -16,6 +18,20 @@ import { MaterialModule } from 'src/app/shared/modules/material/material.module'
 })
 export class MedicalCoverageComponent {
   panelOpenState = true;
+
+  isSmallScreen = false;
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver) { }
+  
+  ngOnInit(): void {
+    this.breakpointObserver.observe('(max-width: 775px)')    
+      .subscribe((state: BreakpointState) => {
+        if (state.matches)
+          this.isSmallScreen = true;
+        else
+          this.isSmallScreen = false;    
+      });  
+  }  
+
   
   @Input({required: true}) accountType: string = '';
   @Input({required: true}) accountName: string = '';
